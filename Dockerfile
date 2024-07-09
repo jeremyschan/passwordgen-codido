5b0a233b95c8573b05e0b138dcc1211f607699cf
Jeremy Chan <chanc301@teach.cs.utoronto.ca>
2024-07-09 12:08:08 -0400

    final

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,6 @@
 # choose an appropriate base image for your algorithm.
-FROM python:latest
+FROM python:3.10
+FROM --platform=linux/amd64 python:3.10
 
 # docker files start running from the point that got changed. since our code files will tend to change alot,
 # we don't want things like pulling the base docker image and downloading the requirements.txt file to happen everytime.
@@ -12,4 +13,4 @@ COPY . .
 
 ENTRYPOINT ["python", "app.py"]
 
-CMD ["--codido", "False"]
+CMD ["--codido", "False", "--length", "4"]
--- app.py
+++ app.py
@@ -6,49 +6,29 @@ import random
 import string
 
 parser = argparse.ArgumentParser()
-parser.add_argument("--input", help="input")
 parser.add_argument("--output", help="output")
 parser.add_argument("--codido", help="running on codido")
 ############################################
 # TODO: add extra args here
+parser.add_argument("--length", type=int, help="length of password")
 ############################################
 
 args = parser.parse_args()
 
-input_folder_path = os.path.join(os.sep, 'app', 'inputs')
 output_folder_path = os.path.join(os.sep, 'app', 'outputs')
-os.makedirs(input_folder_path, exist_ok=True)
 os.makedirs(output_folder_path, exist_ok=True)
 
-if args.codido == 'True':
-    import boto3
-    s3 = boto3.client('s3')
-
-    # downloads codido input file into the folder specified by input_folder_path
-    input_file_path = os.path.join(input_folder_path, args.input.split('_SPLIT_')[-1])
-    s3.download_file(os.environ['S3_BUCKET'], args.input, input_file_path)
-else:
-    input_file_path = glob.glob(os.path.join(input_folder_path, '*'))[0]
-
 ############################################
-# TODO: the input is now accessible via input_file_path
 ############################################
 
 # get filename and filepath first
-for folder_name, subfolders, filenames in os.walk('./inputs'):
-    for filename in filenames:
-        file_path = folder_name + "/" + filename
+pw_length = int(args.length) if args.length else 4
 
-# read contents of file
-with open(file_path, 'r') as file:
-    file_content = file.read().strip()
-
-pw_length = int(file_content)
 
 def generate_password(length):
     password_chars = [
         random.choice(string.ascii_uppercase),
-        random.choice(string.ascii.lowercase),
+        random.choice(string.ascii_lowercase),
         random.choice(string.digits),
         random.choice(string.punctuation)
     ]
@@ -58,6 +38,7 @@ def generate_password(length):
 
     random.shuffle(password_chars)
     return ''.join(password_chars)
+
 
 password = generate_password(pw_length)
 print(password)
@@ -78,4 +59,6 @@ if args.codido == 'True':
                 zip_ref.write(file_path, arcname=os.path.relpath(file_path, output_folder_path))
 
     # upload
+    import boto3
+    s3 = boto3.client('s3')
     s3.upload_file(zip_name, os.environ['S3_BUCKET'], args.output)
